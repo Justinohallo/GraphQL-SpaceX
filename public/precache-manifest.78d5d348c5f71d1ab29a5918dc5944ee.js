e5b3fda94fe305a0dc72c352f28e43f77ba11405
Justin O'Halloran <justinohallo@gmail.com>
2019-03-12 11:21:47 -0700

    Finished React

---
+++ public/precache-manifest.78d5d348c5f71d1ab29a5918dc5944ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b15ef94746031f8853af907909881f2a",
+    "url": "/static/media/logo.b15ef947.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9cb67bcfdf547c5b2dbc",
+    "url": "/static/js/main.a1ee996e.chunk.js"
+  },
+  {
+    "revision": "e20acb0dfb4a33dfc0a1",
+    "url": "/static/js/2.adb9bb66.chunk.js"
+  },
+  {
+    "revision": "9cb67bcfdf547c5b2dbc",
+    "url": "/static/css/main.e93717a2.chunk.css"
+  },
+  {
+    "revision": "973d57991557bb8da929d5842a67e332",
+    "url": "/index.html"
+  }
+];
